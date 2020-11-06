a035dabd84cd293f9fb284812614362b4ea287fa
Carlos Devia <carlosdevia@imaginacolombia.com>
2020-11-06 15:10:11 -0500

    Update: Dimsav\Translatable to Astrotomic\Translatable

--- Modules/Iprofile/Entities/Address.php
+++ Modules/Iprofile/Entities/Address.php
@@ -2,7 +2,7 @@
 
 namespace Modules\Iprofile\Entities;
 
-use Dimsav\Translatable\Translatable;
+use Astrotomic\Translatable\Translatable;
 use Illuminate\Database\Eloquent\Model;
 use Modules\Fhia\Entities\BranchOffice;
 use Modules\User\Entities\Sentinel\User;
