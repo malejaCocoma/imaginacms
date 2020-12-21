1baf54ca59f03737ac3cd0a3fc775ac66b2be782
Juan Escalante <jcec007@gmail.com>
2020-12-21 10:23:01 -0500

    +Delete: Custom Setting in the Icustom

--- Modules/Icustom/Config/settings-fields.php
+++ Modules/Icustom/Config/settings-fields.php
@@ -1,23 +1,6 @@
 <?php
 
 return [
-  'custom-featured-categories-home' => [
-    'name' => 'icustom::custom-featured-categories-home',
-    'value' => null,
-    'type' => 'treeSelect',
-    'columns' => 'col-12 col-md-6',
-    'loadOptions' => [
-      'apiRoute' => 'apiRoutes.qcommerce.categories',
-      'select' => ['label' => 'title', 'id' => 'id'],
-    ],
-    'props' => [
-      'label' => 'icustom::common.custom-featured-categories-home',
-      'clearable' => true,
-      'multiple' => true,
-      'value-consists-of' => 'BRANCH_PRIORITY',
-      'sort-value-by' => 'ORDER_SELECTED'
-    ],
-  ],
 
 
 ];
