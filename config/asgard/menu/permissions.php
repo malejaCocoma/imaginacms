2e32f17e9d05f1e6100c314867ffb851645f6bb3
Juan Escalante <jcec007@gmail.com>
2021-02-02 19:43:26 -0500

    +Update: Modules Public config Permissions

--- config/asgard/menu/permissions.php
+++ config/asgard/menu/permissions.php
@@ -1,16 +1,18 @@
 <?php
 
 return [
-    'menu.menus' => [
-        'index' => 'menu::menu.list resource',
-        'create' => 'menu::menu.create resource',
-        'edit' => 'menu::menu.edit resource',
-        'destroy' => 'menu::menu.destroy resource',
-    ],
-    'menu.menuitems' => [
-        'index' => 'menu::menu-items.list resource',
-        'create' => 'menu::menu-items.create resource',
-        'edit' => 'menu::menu-items.edit resource',
-        'destroy' => 'menu::menu-items.destroy resource',
-    ],
+  'menu.menus' => [
+    'manage' => 'menu::menu.manage resource',
+    'index' => 'menu::menu.list resource',
+    'create' => 'menu::menu.create resource',
+    'edit' => 'menu::menu.edit resource',
+    'destroy' => 'menu::menu.destroy resource',
+  ],
+  'menu.menuitems' => [
+    'manage' => 'menu::menu-items.manage resource',
+    'index' => 'menu::menu-items.list resource',
+    'create' => 'menu::menu-items.create resource',
+    'edit' => 'menu::menu-items.edit resource',
+    'destroy' => 'menu::menu-items.destroy resource',
+  ],
 ];
